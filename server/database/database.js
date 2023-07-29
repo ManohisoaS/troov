a114401182bff5a02c2e786c89c89faae9091cf1
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-07-29 08:24:25 +0300

    create database connexion with mongoose

---
+++ server/database/database.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+/**
+ * Connect to a mongodb URL
+ *
+ * @param {string} url - The connection URL to the MongoDB database.
+ * @param {Object} [options] - The connection options to the MongoDB database.
+ * @returns {Promise<mongoose.Connection>} A promise that resolves with an object `mongoose.Connection` representing the connection to the MongoDB database.
+ * @throws {Error} Throws an error if the connection to the MongoDB database fails.
+ */
+async function connect(url, options = {}){
+  try {
+    // Connects to the MongoDB database using Mongoose.
+    mongoose.connection.on("connecting", () => {
+      console.log("MongoDB: connecting...");
+    });
+    mongoose.connection.on("connected", () => {
+      console.log("MongoDB: connected.");
+    });
+    mongoose.connection.on("disconnecting", () => {
+      console.log("MongoDB: disconnecting...");
+    });
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB: disconnected.");
+    });
+
+    return await mongoose.connect(url, ...options);
+  } catch (error) {
+    console.error("Error while trying to MongoDB database:", error);
+    throw error;
+  }
+}
+
+module.exports = connect;
