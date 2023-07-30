f3952147988d5e08356fa29da967923a4f5c364d
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-07-30 21:58:16 +0300

    add documentation for not found controller

--- server/src/controllers/notFound.js
+++ server/src/controllers/notFound.js
@@ -1,3 +1,11 @@
+/**
+ * Not found request.
+ * @async
+ * @function
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @returns {Object} - An objects with response header 404.
+ */
 function notFoundController(req, res) {
   res.status(404).json({
     message: "Page not found",
