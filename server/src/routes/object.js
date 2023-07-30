c5d4f999c8aed89dae75627ea7b84d2e9b390085
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-07-30 18:25:14 +0300

    add update object:id route

--- server/src/routes/object.js
+++ server/src/routes/object.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const authenticateToken = require("../utils/authentication");
 const createController = require("../controllers/object/createController");
 const getController = require("../controllers/object/getController");
+const updateController = require("../controllers/object/updateController");
 
 // create new object
 router.post("/object", authenticateToken, createController);
 router.get("/objects", authenticateToken, getController);
+router.put("/object/:id", authenticateToken, updateController);
 
 module.exports = router;
