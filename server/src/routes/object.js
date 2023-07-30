d91f982840f3ffea0c38a4084ed422f3cd4549b0
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-07-30 17:09:35 +0300

    add route get objects

--- server/src/routes/object.js
+++ server/src/routes/object.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authenticateToken = require("../utils/authentication");
 const createController = require("../controllers/object/createController");
+const getController = require("../controllers/object/getController");
 
 // create new object
 router.post("/object", authenticateToken, createController);
+router.get("/objects", authenticateToken, getController);
 
 module.exports = router;
