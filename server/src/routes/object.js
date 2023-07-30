c10689a0cf8d8f015e34deb3ae7afe16f10f7670
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-07-30 21:20:56 +0300

    add delete object route

--- server/src/routes/object.js
+++ server/src/routes/object.js
@@ -5,10 +5,12 @@ const authenticateToken = require("../utils/authentication");
 const createController = require("../controllers/object/createController");
 const getController = require("../controllers/object/getController");
 const updateController = require("../controllers/object/updateController");
+const deletedObject = require("../controllers/object/deleteController");
 
 // create new object
 router.post("/object", authenticateToken, createController);
 router.get("/objects", authenticateToken, getController);
 router.put("/object/:id", authenticateToken, updateController);
+router.delete("/object/:id", authenticateToken, deletedObject);
 
 module.exports = router;
