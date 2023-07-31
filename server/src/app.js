d9becab1c79557afb4e054dd0297d8cd2603de21
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-07-31 23:06:36 +0300

    add cors domain

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config({ path: path.join(__dirname,"..", ".env") });
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 var userRouter = require("./routes/user");
 var objectRouter = require("./routes/object");
 var notFoundController = require("./controllers/notFound");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
