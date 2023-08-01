fed8a0e13073ed9b0d8fcb58a4aceff455b5de0b
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-08-01 08:10:31 +0300

    add default layout and updtade login page to use it

---
+++ client/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container-fluid">
+        <div class="row bg-light d-flex justify-content-center align-items-center">
+            <div
+                class="col d-flex justify-content-center align-items-center col-12 col-md-8 col-lg-6 bg-light text-dark min-vh-100">
+                <nuxt />
+            </div>
+        </div>
+    </div>
+</template>
