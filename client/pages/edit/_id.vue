6b47fe3e61d5b2ed941970de4e030f9d0c605488
Manohisoa Sarobidy RANDRIANANTENAINA <manohisoa.randrianantenaina.com>
2023-08-01 14:13:43 +0300

    update edit page

--- client/pages/edit/_id.vue
+++ client/pages/edit/_id.vue
@@ -9,7 +9,7 @@
             {{ successMessage }}
         </div>
 
-        <form @submit.prevent="handleSubmit" class="mt-4">
+        <form @submit.prevent="handleUpdate" class="mt-4">
             <div class="form-group">
                 <label for="nameInput" class="text-left">Name</label>
                 <input type="text" class="form-control" id="nameInput" placeholder="Object name" required v-model="name" />
@@ -42,24 +42,93 @@ export default {
     layout: "manager",
     data() {
         return {
-            name: "",
-            description: "",
             submitting: false,
             errorMessage: null,
             successMessage: null,
         }
     },
 
-    fetch({ params }) {
+    // Load information about object to inputs fields
+    async asyncData({ $config, params, redirect }) {
         const id = params.id;
-        console.log("ID : ", id);
         if (id) {
-            //  get object information 
+            const token = localStorage.getItem('access_token');
+            const response = await fetch(`${$config.baseURL}/object/${id}`, {
+                method: "GET",
+                headers: {
+                    authorization: `Bearer ${token}`,
+                },
+            });
+
+            const data = await response.json();
+            if (data.success) {
+                return {
+                    name: data.object.name,
+                    description: data.object.description,
+                    id
+                }
+            }
+            else {
+                // Navigate to home screen
+                redirect("/");
+            }
         }
         else {
-            // Navigate to home screen
-            this.$router.push("/");
+            // Navigate to home screen if id is not defined in URL
+            redirect("/");
         }
+    },
+    methods:{
+        async handleUpdate() {
+            try {
+                // Reset error message
+                this.errorMessage = null;
+
+                // Form is submitting
+                this.submitting = true;
+
+                // Send login request
+                const token = localStorage.getItem('access_token');
+                const response = await fetch(`${this.$config.baseURL}/object/${this.id}`, {
+                    method: "PUT",
+                    body: JSON.stringify({
+                        name: this.name,
+                        description: this.description,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${token}`,
+                    },
+                });
+
+                const data = await response.json();
+
+                if (data.success) {
+                    this.successMessage = `"${data.updatedObject.name}" object updated`;
+                    // update inputs
+                    this.name = data.updatedObject.name;
+                    this.description = data.updatedObject.name;
+                }
+                else {
+                    // Display error message returning by server
+                    this.errorMessage = data.error;
+                }
+
+            } catch (error) {
+                // Display error message in case of error
+                if (
+                    error.data &&
+                    error.data.error
+                ) {
+                    this.errorMessage = error.data.error;
+                } else {
+                    this.errorMessage = "Something went wrong.";
+                }
+            } finally {
+                // Form is no longer submitting
+                this.submitting = false;
+            }
+        },
     }
 }
 </script>
